537e56cf03df1c99e892969d2b8f144459983ab1
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-05-30 18:16:58 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,42 +1,20 @@
-#ProjectSakura building script for crave
-#!/bin/bash
+#ProjectSakura build script for crave
 
-set -e
-export PROJECTFOLDER="Lineage21"
-export PROJECTID="72"
-export REPO_INIT="repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs --depth=1"
-export BUILD_DIFFERENT_ROM="repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs" # Change this if you'd like to build something else
+#Remove existing local_mainfests
+rm -rf .repo/local_manifests/
 
-# Destroy Old Clones
-if (grep -q "$PROJECTFOLDER" <(crave clone list --json | jq -r '.clones[]."Cloned At"')) && [ "${DCDEVSPACE}" == "1" ]; then   
-   crave clone destroy -y /crave-devspaces/$PROJECTFOLDER || echo "Error removing $PROJECTFOLDER"
-else
-   rm -rf $PROJECTFOLDER || true
-fi
-
-# Create New clone
-if [ "${DCDEVSPACE}" == "1" ]; then
-   crave clone create --projectID $PROJECTID /crave-devspaces/$PROJECTFOLDER || echo "Crave clone create failed!"
-   cd /crave-devspaces/$PROJECTFOLDER
-else
-   mkdir $PROJECTFOLDER
-   cd $PROJECTFOLDER
-   echo "Running $REPO_INIT"
-   $REPO_INIT
-fi
-
-# Run inside foss.crave.io devspace
-# Remove existing local_manifests
-crave run --no-patch -- "rm -rf .repo/local_manifests && \
-
-# Init Manifest
-$BUILD_DIFFERENT_ROM && \
+#ProjectSakura repo init
+repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs
 
 # Clone local_manifests repository
-git clone https://github.com/AsTechpro20/local_manifests -b sakura-14 .repo/local_manifests && \
+git clone https://github.com/AsTechpro20/local_manifests -b main .repo/local_manifests && \
 
- # Sync the repositories
- /opt/crave/resync.sh && \ 
+#Details
+export BUILD_USERNAME=AsTechpro20
+export BUILD_HOSTNAME=crave
+
+# Sync the repositories
+/opt/crave/resync.sh && \ 
 
 # Set up build environment
 source build/envsetup.sh && \
@@ -44,14 +22,5 @@ source build/envsetup.sh && \
 # Lunch configuration
 lunch lineage_mi439-ap1a-userdebug && \
 
-# Build the ROM
+# Build the ProjectSakura
 mka bacon"
-
-cd ..
-
-# Clean up
-if grep -q "$PROJECTFOLDER" <(crave clone list --json | jq -r '.clones[]."Cloned At"') && [ "${DCDEVSPACE}" == "1" ]; then
-  crave clone destroy -y /crave-devspaces/$PROJECTFOLDER || echo "Error removing $PROJECTFOLDER"
-else  
-  rm -rf $PROJECTFOLDER || true
-fi
