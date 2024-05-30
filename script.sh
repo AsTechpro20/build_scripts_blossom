16aca46aaf226df9e9e067a7a2d78e58b000a1a6
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-05-30 15:59:52 +0530

    script.sh: Import my script
    Source:
    
    https://github.com/sounddrill31/android_build_scripts/blob/lineage-oxygen/build.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,57 @@
-#ProjectSakura building scrpit for crave
+#ProjectSakura building script for crave
+#!/bin/bash
 
-rm -rf .repo/local_manifests/
+set -e
+export PROJECTFOLDER="Lineage21"
+export PROJECTID="72"
+export REPO_INIT="repo init -u https://github.com/accupara/los20.git -b lineage-20.0 --git-lfs --depth=1"
+export BUILD_DIFFERENT_ROM="repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs" # Change this if you'd like to build something else
 
-#Repo init rom
-repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs
+# Destroy Old Clones
+if (grep -q "$PROJECTFOLDER" <(crave clone list --json | jq -r '.clones[]."Cloned At"')) && [ "${DCDEVSPACE}" == "1" ]; then   
+   crave clone destroy -y /crave-devspaces/$PROJECTFOLDER || echo "Error removing $PROJECTFOLDER"
+else
+   rm -rf $PROJECTFOLDER || true
+fi
 
-#Local manifests
-git clone https://github.com/AsTechpro20/local_manifests -b sakura-14 .repo/local_manifests
+# Create New clone
+if [ "${DCDEVSPACE}" == "1" ]; then
+   crave clone create --projectID $PROJECTID /crave-devspaces/$PROJECTFOLDER || echo "Crave clone create failed!"
+   cd /crave-devspaces/$PROJECTFOLDER
+else
+   mkdir $PROJECTFOLDER
+   cd $PROJECTFOLDER
+   echo "Running $REPO_INIT"
+   $REPO_INIT
+fi
 
-#build
-/opt/crave/resync.sh
-. build/envsetup.sh
-lunch lineage_mi439-ap1a-userdebug
-mka baco
+# Run inside foss.crave.io devspace
+# Remove existing local_manifests
+crave run --no-patch -- "rm -rf .repo/local_manifests && \
+
+# Init Manifest
+$BUILD_DIFFERENT_ROM && \
+
+# Clone local_manifests repository
+git clone https://github.com/AsTechpro20/local_manifests -b sakura-14 .repo/local_manifests && \
+
+ # Sync the repositories
+ /opt/crave/resync.sh && \ 
+
+# Set up build environment
+source build/envsetup.sh && \
+
+# Lunch configuration
+lunch lineage_mi439-ap1a-userdebug && \
+
+# Build the ROM
+mka bacon"
+
+cd ..
+
+# Clean up
+if grep -q "$PROJECTFOLDER" <(crave clone list --json | jq -r '.clones[]."Cloned At"') && [ "${DCDEVSPACE}" == "1" ]; then
+  crave clone destroy -y /crave-devspaces/$PROJECTFOLDER || echo "Error removing $PROJECTFOLDER"
+else  
+  rm -rf $PROJECTFOLDER || true
+fi
