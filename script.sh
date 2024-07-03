75db71b2b65a6f72bb90f6166e8e45b14439640d
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-07-03 12:40:07 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ rm -rf .repo/local_manifests/ || { echo "Failed to remove local_manifests"; exit
 
 # Initialize ProjectSakura repo
 echo "Initializing ProjectSakura repo..."
-repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs || { echo "Repo init failed"; exit 1; }
+repo init -u https://github.com/alphadroid-project/manifest -b alpha-14 --git-lfs || { echo "Repo init failed"; exit 1; }
 
 # Clone local_manifests repository
 echo "Cloning local_manifests repository..."
@@ -21,25 +21,13 @@ export BUILD_HOSTNAME=crave
 echo "Syncing the repositories..."
 /opt/crave/resync.sh || { echo "Resync failed"; exit 1; }
 
-#Change to the vendor/lineage directory
-echo "Change to the build/soong directory"
-cd vendor/lineage || { echo "Directory not found: build/soong"; exit 1; }
-
-#Applying patch
-echo "Applying patches..."
-curl  https://github.com/AsTechpro20/vendor_sakura/commit/34e5228d5af48e843932df7da825672bca675f49.patch | git am || { echo "Failed to apply the patch"; }
-
-#Back to main directory
-echo "Returning to the main directory"
-cd ../.. || { echo "Failed to return to the main directory"; exit 1; }
-
 # Initialize repo with Git LFS
 echo "Reinitializing repo with Git LFS..."
 repo forall -vc "git lfs pull" || { echo "Repo re-init with Git LFS failed"; exit 1; }
 
 # Set up build environment
 echo "Setting up build environment..."
-source build/envsetup.sh || { echo "Envsetup failed"; exit 1; }
+. build/envsetup.sh || { echo "Envsetup failed"; exit 1; }
 
 # Lunch configuration
 echo "Configuring lunch..."
@@ -47,6 +35,7 @@ lunch lineage_mi439-ap1a-userdebug || { echo "Lunch configuration failed"; exit 
 
 # Build the ProjectSakura
 echo "Building ProjectSakura..."
-mka bacon || { echo "Build failed"; exit 1; }
+make bacon || { echo "Build failed"; exit 1; }
 
+brunch mi439
 echo "Build completed successfully!"
