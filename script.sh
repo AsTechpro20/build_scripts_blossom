f80d7be51b648cba5899b6c55c2f1f4754dff4f8
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-07-21 08:07:51 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#Lineage-18.1 build script
+#Lineage-21 build script
 
 # Remove existing local_manifests
 echo "Removing existing local_manifests..."
@@ -6,11 +6,11 @@ rm -rf .repo/local_manifests/
 
 # Clone local_manifests repository
 echo "Cloning local_manifests repository..."
-git clone https://github.com/AsTechpro20/local_manifests.git -b Lineage-18.1 .repo/local_manifests
+git clone https://github.com/AsTechpro20/local_manifests.git -b Lineage-21 .repo/local_manifests
 
 # Initialize Lineage-18.1 repo
 echo "Initializing Lineage-18.1 repo..."
-repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 
 # Sync the repositories
 echo "Syncing the repositories..."
@@ -20,8 +20,8 @@ echo "Syncing the repositories..."
 echo "Setting up build environment..."
 . build/envsetup.sh
 
-# Brunch configuration and Building Lineage-18.1
-echo "Configuring brunch and building Lineage-18.1..."
+# Brunch configuration and Building Lineage-21
+echo "Configuring brunch and building Lineage-21..."
 brunch Mi439
 
 echo "Build completed successfully!"
