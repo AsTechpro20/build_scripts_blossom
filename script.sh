610ed5abb37900c06d5eb5ca8407589db2d5b21e
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-09-02 07:38:16 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#Lineage-21 build_script_blossom
+#AlphaDroid-14 build_script_blossom
 
 # Remove some stuffs
 rm -rf .repo/local_manifests
@@ -12,15 +12,15 @@ echo "Removing stuffs success..."
 echo "===================================="
 
 # Clone local_manifests repository
-git clone https://github.com/AsTechpro20/local_manifests_blossom.git -b Lineage-21 .repo/local_manifests
+git clone https://github.com/AsTechpro20/local_manifests_blossom.git -b alphadroid-14 .repo/local_manifests
 echo "===================================="
 echo "Cloning local_manifests was success..."
 echo "===================================="
 
-# Initialize Lineage-21 repo
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+# Initialize AlphaDroid-14 repo
+repo init -u https://github.com/alphadroid-project/manifest -b alpha-14 --git-lfs
 echo "===================================="
-echo "Initializing Lineage-21 repo was success..."
+echo "Initializing AlphaDroid-14 repo was success..."
 echo "===================================="
 
 # Sync the repositories
@@ -42,9 +42,9 @@ echo "Setting up build environment..."
 echo "===================================="
 source build/envsetup.sh
 
-#Building Lineage-21
+#Building AlphaDroid-14
 echo "===================================="
-echo "Building Lineage-21..."
+echo "Building AlphaDroid-14..."
 echo "===================================="
 brunch blossom || brunch blossom-userdebug || lunch lineage_blossom-ap2a-userdebug && mka bacon  
  
