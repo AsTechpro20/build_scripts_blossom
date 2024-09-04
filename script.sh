0d31af696d4b641397b77aba72030ce3eb9c9b01
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-09-04 14:05:49 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,11 @@
 
 # Remove some stuffs
 rm -rf .repo/local_manifests
+rm -rf  vendor/xiaomi
+rm -rf  kernel/xiaomi
+rm -rf  device/xiaomi
+rm -rf  device/mediatek
+rm -rf  hardware/mediatek
 echo "===================================="
 echo "Removing stuffs success..."
 echo "===================================="
@@ -16,12 +21,6 @@ echo "===================================="
 git clone https://github.com/AsTechpro20/local_manifests_blossom.git -b afterlife-14 .repo/local_manifests
 echo "===================================="
 echo "Cloning local_manifests was success..."
-echo "===================================="
-
-# Initialize AfterLife-14 repo
-repo init --depth=1 -u https://github.com/AfterlifeOS/android_manifest.git -b 14 --git-lfs
-echo "===================================="
-echo "Initializing AfterLife-14 repo was success..."
 echo "===================================="
 
 # Sync the repositories
@@ -43,4 +42,3 @@ echo "Building AfterLife-14..."
 echo "===================================="
 . build/envsetup.sh
 goafterlife blossom 
- 
